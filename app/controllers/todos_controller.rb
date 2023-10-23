59856299cf50f27d8d3ade4b9650759e174d2c99
KentY <e6ony.ivory@gmail.com>
2023-10-24 00:00:58 +0900

    Todo追加機能実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,24 @@ class TodosController < ApplicationController
     def index
         @todos = Todo.all
     end
+
+    def new
+        @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+            if
+                @todo.save
+                puts "成功しました"
+                redirect_to todos_path
+            else
+                puts "失敗しました"
+            end
+    end
+
+    private
+        def todo_params
+            params.permit(:text)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # ''は初期アクセス時のルートは何かを表している
   get '', to: 'todos#index'
   get 'todos', to: 'todos#index'
+  post 'todos' , to: 'todos#create'
 end
